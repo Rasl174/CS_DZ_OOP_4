14037187e5dbdd03e12d69fe48ac3767f2df0fc7
Rasl174 <98830529+Rasl174@users.noreply.github.com>
2022-04-06 12:17:04 +0500

    Изменил

--- CS_DZ_OOP_4/Program.cs
+++ CS_DZ_OOP_4/Program.cs
@@ -23,7 +23,7 @@ namespace CS_DZ_OOP_4
     {
         private List<Deck> _playerDeck = new List<Deck>();
         private Queue<Deck> _cardsInHend = new Queue<Deck>();
-        
+
         public Player(List<Deck> playerDeck)
         {
             _playerDeck = playerDeck;
@@ -49,12 +49,13 @@ namespace CS_DZ_OOP_4
                         TakeDeck();
                         break;
                     case "2":
-                        //TryShuffleDeck();
+                        Shuffle();
                         break;
                     case "3":
                         TakeOneCard();
                         break;
                     case "4":
+                        ShowPlayerCards();
                         break;
                     case "5":
                     case "exit":
@@ -66,38 +67,46 @@ namespace CS_DZ_OOP_4
             }
         }
 
+        private void ShowPlayerCards()
+        {
+            foreach (var item in _playerDeck)
+            {
+                _cardsInHend.Enqueue(item);
+                item.ShowCards();
+            }
+        }
+
         private void TakeDeck()
         {
             if (_playerDeck.Count > 0)
             {
                 Console.WriteLine("В руках уже есть колода.");
-                Console.Clear();
                 Console.ReadKey();
+                Console.Clear();
             }
             else
             {
-                
+                _playerDeck.Add(new Deck());
             }
         }
 
         private void TakeOneCard()
         {
 
-            Console.WriteLine(_cardsInHend.Dequeue());
         }
 
-        private bool TryShuffleDeck(out Deck deck)
+        private void Shuffle()
         {
-            deck = null;
-
-            if (_playerDeck.Count > 0)
+            if (_playerDeck.Count == 1)
             {
-                deck.TakeDeck();
-                return true;
+                foreach (var deck in _playerDeck)
+                {
+                    deck.ShuffleDeck(_playerDeck);
+                }
             }
             else
             {
-                return false;
+                Console.WriteLine("В руках нет колоды!");
             }
         }
     }
@@ -105,6 +114,47 @@ namespace CS_DZ_OOP_4
     class Deck
     {
         private List<Card> _cards = new List<Card>();
+
+        public Deck()
+        {
+            TryTakeDeck();
+        }
+
+        public void TryTakeDeck()
+        {
+            if (_cards.Count > 0)
+            {
+                Console.WriteLine("    wefwfwefwe    ");
+            }
+            else
+            {
+                TakeDeck();
+            }
+        }
+
+        public void ShowCards()
+        {
+            foreach (var item in _cards)
+            {
+                Console.WriteLine(item.CardFace);
+            }
+        }
+
+        public void ShuffleDeck<Deck>(List<Deck> cards)
+        {
+            Console.WriteLine("Колода перемешана!");
+
+            Random random = new Random();
+
+            for (int i = _cards.Count - 1; i >= 1; i--)
+            {
+                int j = random.Next(i + 1);
+
+                Deck temp = cards[j];
+                cards[j] = cards[i];
+                cards[i] = temp;
+            }
+        }
 
         public void TakeDeck()
         {
@@ -117,29 +167,6 @@ namespace CS_DZ_OOP_4
             _cards.Add(new Card("Дама - буби")); _cards.Add(new Card("Дама - пики")); _cards.Add(new Card("Дама - черви")); _cards.Add(new Card("Дама - крести"));
             _cards.Add(new Card("Король - буби")); _cards.Add(new Card("Король - пики")); _cards.Add(new Card("Король - черви")); _cards.Add(new Card("Король - крести"));
             _cards.Add(new Card("Туз - буби")); _cards.Add(new Card("Туз - пики")); _cards.Add(new Card("Туз - черви")); _cards.Add(new Card("Туз - крести"));
-        }
-
-        public void ShuffleDeck<Deck>(List<Deck> cards)
-        {
-            if (_cards.Count > 0)
-            {
-                Console.WriteLine("Колода перемешана!");
-
-                Random rand = new Random();
-
-                for (int i = cards.Count - 1; i >= 1; i--)
-                {
-                    int j = rand.Next(i + 1);
-
-                    Deck temp = cards[j];
-                    cards[j] = cards[i];
-                    cards[i] = temp;
-                }
-            }
-            else
-            {
-                Console.WriteLine("В руках нет колоды!");
-            }
         }
     }
 
